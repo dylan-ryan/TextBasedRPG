079b2b4c34dd0a5e305cd25883ac24d57f426db3
dylan-ryan <dylanryan431@gmail.com>
2023-11-28 21:24:24 -0400

    spectacular advancements

--- Program.cs
+++ Program.cs
@@ -6,23 +6,30 @@ using System.Threading.Tasks;
 using System.IO; // File.*
 using System.Threading;
 using System.Web;
+using System.Xml;
 
 namespace TextBasedRPG
 {
     internal class Program
     {
-
+        static int enHealth = 2;
+        static int health = 3;
         static char avatar = '@';
         static bool gameOver = false;
-        static int x = 30;
-        static int y = 10;
-        static char[,] map;
+        static int x = 28;
+        static int y = 8;
+
+        static char enemy = '&';
+        static int enX = 10;
+        static int enY = 4;
         
+        static Random r = new Random();
+
+        static char[,] map;
         static void Main(string[] args)
         {
+
             Console.CursorVisible = false;
-            Console.WriteLine("Text Based RPG");
-            Console.WriteLine();
             LoadMap();
 
             //if (map[newX,newY] == '#')
@@ -39,46 +46,206 @@ namespace TextBasedRPG
         }
 
         static void CharacterController()
-        {        
-               ConsoleKeyInfo input;
-               input = Console.ReadKey(true);
-                    if (input.Key == ConsoleKey.W)
-                    {
-                        if (map[x, y] != '#')
-                        {
-                            LoadMap();
-                            y = y - 1;
-                            if (y <= 2) y = 3;
-                        }
-                        else
-                        {
-                            y = y + 1;
-                        }
-                    }
-                    if (input.Key == ConsoleKey.S)
-                    {
-                        LoadMap();
-                        y = y + 1;
-                        if (y > 19) y = 19;
-                    }
-                    if (input.Key == ConsoleKey.D)
-                    {
-                        LoadMap();
-                        x = x + 1;
-                        if (x > 60) x = 60;
-                    }
-                    if (input.Key == ConsoleKey.A)
-                    {
-                        LoadMap();
-                        x = x - 1;
-                        if (x < 1) x = 1;
-                    }
-              Console.SetCursorPosition(x, y);
-              Console.WriteLine(avatar);
+        {  
+
+
+            ConsoleKeyInfo input;
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(avatar);
+            Console.SetCursorPosition(enX, enY);
+            Console.WriteLine(enemy);
+            input = Console.ReadKey(true);
+            if (input.Key == ConsoleKey.W)
+            {
+                y = y - 1;
+                if (map[x, y] == ' ')
+                {
+                    LoadMap();
+                    Console.SetCursorPosition(x, y);
+                    Console.WriteLine(avatar);
+                }
+                else if (map[x, y] == '#')
+                {
+                    LoadMap();
+                    y = y + 1;
+                    Console.SetCursorPosition(x, y);
+                    Console.WriteLine(avatar);
+                }
+
+                //enmy
+                enY = enY + 1;
+                if (map[enX, enY] == ' ')
+                {
+                    Console.SetCursorPosition(enX, enY);
+                    Console.WriteLine(enemy);
+                }
+                else if (map[enX, enY] == '#')
+                {
+                    enY = enY - 1;
+                    Console.SetCursorPosition(enX, enY);
+                    Console.WriteLine(enemy);
+                }
+            }
+            if (input.Key == ConsoleKey.S)
+            {
+                y = y + 1;
+                if (map[x, y] == ' ')
+                {
+                     LoadMap();
+                     Console.SetCursorPosition(x, y);
+                     Console.WriteLine(avatar);
+                }
+                else if (map[x, y] == '#')
+                {
+                    LoadMap();
+                    y = y - 1;
+                }
+                //enmy
+                enY = enY - 1;
+                if (map[enX, enY] == ' ')
+                {
+                    Console.SetCursorPosition(enX, enY);
+                    Console.WriteLine(enemy);
+                }
+                else if (map[enX, enY] == '#')
+                {
+                    enY = enY + 1;
+                }
+            }
+            if (input.Key == ConsoleKey.D)
+            {
+                x = x + 1;
+                if (map[x, y] == ' ')
+                {
+                      LoadMap();
+                      Console.SetCursorPosition(x, y);
+                      Console.WriteLine(avatar);
+
+                }
+                else if (map[x, y] == '#')
+                {
+                    LoadMap();
+                    x = x - 1;
+                }
+                //enmy
+                enX = enX - 1;
+                if (map[enX, enY] == ' ')
+                {
+                    Console.SetCursorPosition(enX, enY);
+                    Console.WriteLine(enemy);
+
+                }
+                else if (map[enX, enY] == '#')
+                {
+                    enX = enX + 1;
+                }
+            }
+            if (input.Key == ConsoleKey.A)
+            {
+                x = x - 1;
+                if (map[x, y] == ' ')
+                {
+                     LoadMap();
+                     Console.SetCursorPosition(x, y);
+                     Console.WriteLine(avatar);
+                }
+                else if (map[x, y] == '#')
+                {
+                    LoadMap();
+                    x = x + 1;
+                }
+                //enmy
+                enX = enX + 1;
+                if (map[enX, enY] == ' ')
+                {
+                    Console.SetCursorPosition(enX, enY);
+                    Console.WriteLine(enemy);
+                }
+                else if (map[enX, enY] == '#')
+                {
+                    enX = enX - 1;
+                }
+                 TakeDamage();
+            }
+            Console.SetCursorPosition(0, 20);
+            Console.WriteLine("Your Health: " + health + "|" + "EnemyHealth: " + enHealth);
+
+            //Console.SetCursorPosition(x, y);
+            //Console.WriteLine(avatar);
+
+           /* Console.SetCursorPosition(enX, enY);
+            Console.WriteLine(enemy);
+            if (input.Key == ConsoleKey.W)
+            {
+                enY = enY - 1;
+                if (map[enX, enY] == ' ')
+                {
+                    Console.SetCursorPosition(enX, enY);
+                    Console.WriteLine(enemy);
+                }
+                else if (map[enX, enY] == '#')
+                {
+                    enY = enY + 1;
+                    Console.SetCursorPosition(enX, enY);
+                    Console.WriteLine(enemy);
+                }
+            }
+            if (input.Key == ConsoleKey.S)
+            {
+                enY = enY + 1;
+                if (map[enX, enY] == ' ')
+                {
+                    Console.SetCursorPosition(enX, enY);
+                    Console.WriteLine(enemy);
+                }
+                else if (map[enX, enY] == '#')
+                {
+                    enY = enY - 1;
+                }
+            }
+            if (input.Key == ConsoleKey.D)
+            {
+                enX = enX + 1;
+                if (map[enX, enY] == ' ')
+                {
+                    Console.SetCursorPosition(enX, enY);
+                    Console.WriteLine(enemy);
+
+                }
+                else if (map[enX, enY] == '#')
+                {
+                    enX = enX - 1;
+                }
+            }
+            if (input.Key == ConsoleKey.A)
+            {
+                enX = enX - 1;
+                if (map[enX, enY] == ' ')
+                {
+                    Console.SetCursorPosition(enX, enY);
+                    Console.WriteLine(enemy);
+                }
+                else if (map[enX, enY] == '#')
+                {
+                    enX = enX + 1;
+                }
+            }*/
+        }
+        static void TakeDamage()
+        {
+            if (map[enX, enY] == map[x, y])
+            {
+                health = health - 1;
+                enHealth = enHealth - 1;
+            }
+            else
+            {
+             
+            }
         }
         static void LoadMap()
         {
-            Console.SetCursorPosition(0, 2);
+            Console.SetCursorPosition (0, 0);
             string path = @"map.txt";
             string[] MapRows;
             MapRows = File.ReadAllLines(path);
@@ -96,5 +263,76 @@ namespace TextBasedRPG
                 Console.WriteLine();
             }
         }
+
+      /* static void EnemyController()
+        {
+            ConsoleKeyInfo input;
+            Console.SetCursorPosition(enX, enY);
+            Console.WriteLine(enemy);
+            input = Console.ReadKey(true);
+            if (input.Key == ConsoleKey.W)
+            {
+                enY = enY - 1;
+                if (map[enX, enY] == ' ')
+                {
+                    LoadMap();
+                    Console.SetCursorPosition(enX, enY);
+                    Console.WriteLine(enemy);
+                }
+                else if (map[enX, enY] == '#')
+                {
+                    enY = enY + 1;
+                    Console.SetCursorPosition(enX, enY);
+                    Console.WriteLine(enemy);
+                }
+            }
+            if (input.Key == ConsoleKey.S)
+            {
+                enY = enY + 1;
+                if (map[enX, enY] == ' ')
+                {
+                    LoadMap();
+                    Console.SetCursorPosition(enX, enY);
+                    Console.WriteLine(enemy);
+                }
+                else if (map[enX, enY] == '#')
+                {
+                    enY = enY - 1;
+                }
+            }
+            if (input.Key == ConsoleKey.D)
+            {
+                enX = enX + 1;
+                if (map[enX, enY] == ' ')
+                {
+                    LoadMap();
+                    Console.SetCursorPosition(enX, enY);
+                    Console.WriteLine(enemy);
+
+                }
+                else if (map[enX, enY] == '#')
+                {
+                    enX = enX - 1;
+                }
+            }
+            if (input.Key == ConsoleKey.A)
+            {
+                enX = enX - 1;
+                if (map[enX, enY] == ' ')
+                {
+                    LoadMap();
+                    Console.SetCursorPosition(enX, enY);
+                    Console.WriteLine(enemy);
+                }
+                else if (map[enX, enY] == '#')
+                {
+                    enX = enX + 1;
+                }
+            }
+            //Console.SetCursorPosition(x, y);
+            //Console.WriteLine(avatar);
+
+
+        }*/
     }
 }
